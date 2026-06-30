dd237ebd0790c6ce24c480078ff85ef1f7422162
Rahul Gupta <rg3498104@gmail.com>
2026-06-30 21:39:24 +0530

    Configured discovery server and config server application.properties and application.yml files

--- config-server/src/main/java/com/rahulsproject/connectu/config_server/ConfigServerApplication.java
+++ config-server/src/main/java/com/rahulsproject/connectu/config_server/ConfigServerApplication.java
@@ -1,9 +1,14 @@
 package com.rahulsproject.connectu.config_server;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
